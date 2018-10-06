8d91febdf177d4a1ec820f0e6c7e34a55339784e
Jeme <aime.urquieta@outlook.com>
2018-10-06 13:30:26 -0500

    added employeeID field

--- assets/javascript/scores.js
+++ assets/javascript/scores.js
@@ -31,7 +31,8 @@ $(document).ready(function () {
         answers: ["Not at all", "Somewhat Difficult", "Very difficult", "Extremely difficult"],
     }];
 
-    var game = {
+    var form = {
+        employeeID,
         totalScore: 0,
         title: "Employee Questionnaire",
         instructions: "Over the last 2 weeks, how often have you been bothered by any of the following problems?",
@@ -40,21 +41,32 @@ $(document).ready(function () {
         count: 0,
     }
 
-    $("#container_1").html(`<h2>${game.title}</h2><br><h6>${game.instructions}</h6>`);
+    $("#container_1").html(`<h2>${form.title}</h2>`);
+
+    $("#employeeID").html(`<div class=row>
+                            <div class="input-field col s3">
+                            <input id="input_text" type="text" data-length="3" maxlength="3" required>
+                            <label for="input_text">Employee ID</label></div></div>`);
+
+
+    $('input#input_text, textarea#textarea2').characterCounter();
+
+
+    $("#form").html(`<h6>${form.instructions}</h6>`);
     //function to display questionnaire questions and asnwers
     function questionsAnswersDisplay(questionnaire) {
         for (var i = 0; i < questionnaire.length; i++) {
-            game.count++;
+            form.count++;
             var question = $('<div class="py-2"><p></p></div>');
             question.attr("id", "question");
             question.text(questionnaire[i].questions).appendTo("#form");
 
             for (var j = 0; j < questionnaire[i].answers.length; j++) {
                 var p = $("<p>");
-                var input = $("<input>").attr("type", "radio").attr("name", game.count).addClass("with-gap").attr("id", questionnaire[i].answers[j]+game.count);
-                var span = $("<span>").addClass("chosen").attr("data", game.count);
+                var input = $("<input>").attr("type", "radio").attr("name", form.count).addClass("with-gap").attr("id", questionnaire[i].answers[j] + form.count);
+                var span = $("<span>").addClass("chosen").attr("data", form.count);
                 span.text(questionnaire[i].answers[j]);
-                var label = $("<label>").attr("for", questionnaire[i].answers[j]+game.count);
+                var label = $("<label>").attr("for", questionnaire[i].answers[j] + form.count);
                 p.append(label.append(input.add(span)));
                 $("#form").append(p);
             }
@@ -63,29 +75,33 @@ $(document).ready(function () {
     }
 
 
+    function cleanData() {
+        $("#input_text").val("");
+        $("input:radio").attr("checked", false);
+    }
     //questionnaire results
     function phq9testResults() {
         switch (true) {
-            case game.totalScore <= 4:
-                game.depStatus = "Minimal Depression";
+            case form.totalScore <= 4:
+                form.depStatus = "Minimal Depression";
                 break;
-            case game.totalScore > 4 && game.totalScore < 10:
-                game.depStatus = "Minimal Depression";
+            case form.totalScore > 4 && form.totalScore < 10:
+                form.depStatus = "Minimal Depression";
                 break;
-            case game.totalScore > 10 && game.totalScore < 15:
-                game.depStatus = "Mild Depression";
+            case form.totalScore > 10 && form.totalScore < 15:
+                form.depStatus = "Mild Depression";
                 break;
-            case game.totalScore > 15 && game.totalScore < 20:
-                game.depStatus = "Moderate Depression";
+            case form.totalScore > 15 && form.totalScore < 20:
+                form.depStatus = "Moderate Depression";
                 break;
-            case game.totalScore > 20 && game.totalScore < 28:
-                game.depStatus = "Severe Depression";
+            case form.totalScore > 20 && form.totalScore < 28:
+                form.depStatus = "Severe Depression";
                 break;
             default:
-                game.depStatus = "Employee has not completed PHQ-9 assesment.";
+                form.depStatus = "Employee has not completed PHQ-9 assesment.";
         }
-        game.depStatus = $(`<script>${game.depStatus}</script>`);
-        console.log(game.depStatus);
+        form.depStatus = $(`<script>${form.depStatus}</script>`);
+        console.log(form.depStatus);
     }
 
     $(document.body).on("click", '.chosen', function () {
@@ -95,24 +111,27 @@ $(document).ready(function () {
             for (var j = 0; j < questionnaire[i].answers.length; j++) {
                 if (chosen.text() === questionnaire[i].answers[j] && parseInt(chosen.attr("data")) === i) {
                     questionnaire[i].score = j;
-                    game.sumScoreArray.push(questionnaire[i].score);
+                    form.sumScoreArray.push(questionnaire[i].score);
                 }
             }
         }
     })
 
     function getScore() {
-        for (var k = 0; k < game.sumScoreArray.length; k++) {
-            game.totalScore += game.sumScoreArray[k];
+        for (var k = 0; k < form.sumScoreArray.length; k++) {
+            form.totalScore += form.sumScoreArray[k];
         }
-        console.log(game.totalScore);
-        return game.totalScore;
+        console.log(form.totalScore);
+        return form.totalScore;
     }
 
     $("#submitB").on("click", function (event) {
         event.preventDefault();
+        form.employeeID = parseInt($("#input_text").val().trim());
         getScore();
         phq9testResults();
+        //getDepressionResult(iEmpNum);
+        cleanData();
     });
 
     questionsAnswersDisplay(questionnaire);
